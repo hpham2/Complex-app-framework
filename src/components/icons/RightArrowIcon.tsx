8d8a4c5e76f58755670c9b709d8d694f8a4f32c0
hpham2 <phamhoangtrunghai@gmail.com>
2024-06-12 16:19:18 +0200

    Add icons

---
+++ src/components/icons/RightArrowIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export function RightArrowIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      height="1.5em"
+      width="1.5em"
+      style={{ marginLeft: "8px" }}
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M1 8a7 7 0 1014 0A7 7 0 001 8zm15 0A8 8 0 110 8a8 8 0 0116 0zM4.5 7.5a.5.5 0 000 1h5.793l-2.147 2.146a.5.5 0 00.708.708l3-3a.5.5 0 000-.708l-3-3a.5.5 0 10-.708.708L10.293 7.5H4.5z"
+      />
+    </svg>
+  );
+}
